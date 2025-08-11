aa9026349a06a232a61cfc34dae2fd2a8182f22a
Jimmy Lynch <jlyn2195@gmail.com>
2025-08-11 15:42:50 -0700

    added comments

--- run_fit_point_source.py
+++ run_fit_point_source.py
@@ -17,33 +17,9 @@ from helper_functions import fit_point_source
 prefix = "/Users/jimmylynch/Desktop/radio/observations/23A-241/23A-241.ASASSN-14ae.2023-09-17/EVLA_L"
 image_paths = [f"{prefix}/1.25GHz/23A-241.ASASSN-14ae.2023-09-17.1.25GHz.2700px.image.tt0",
                f"{prefix}/1.5GHz/23A-241.ASASSN-14ae.2023-09-17.1.5GHz.2700px.image.tt0",
-               f"{prefix}/1.75GHz/23A-241.ASASSN-14ae.2023-09-17.1.75GHz.2700px.image.tt0"]
-
-'''
-image_paths = [f"{prefix}/EVLA_L/1.25GHz/23A-241.ASASSN-14ae.2023-07-23.1.25GHz.2700px.image.tt0",
-               f"{prefix}/EVLA_L/1.5GHz/23A-241.ASASSN-14ae.2023-07-23.1.5GHz.2700px.image.tt0",
-               f"{prefix}/EVLA_L/1.75GHz/23A-241.ASASSN-14ae.2023-07-23.1.75GHz.2700px.image.tt0",
-               f"{prefix}/EVLA_S/2.5GHz/23A-241.ASASSN-14ae.2023-07-23.2.5GHz.2700px.image.tt0",
-               f"{prefix}/EVLA_S/3.0GHz/23A-241.ASASSN-14ae.2023-07-23.3.0GHz.2700px.image.tt0",
-               f"{prefix}/EVLA_S/3.5GHz/23A-241.ASASSN-14ae.2023-07-23.3.5GHz.2700px.image.tt0"]
-
-
-image_paths = [f"{prefix}/5.0GHz/Target_ASASSN_14ae_EVLA_C_final.image.tt0",
-               f"{prefix}/6.0GHz/Target_ASASSN_14ae_EVLA_C_final.image.tt0",
-               f"{prefix}/7.0GHz/Target_ASASSN_14ae_EVLA_C_final.image.tt0",
-               f"{prefix}/9.0GHz/Target_ASASSN_14ae_EVLA_X_final.image.tt0",
-               f"{prefix}/10.0GHz/Target_ASASSN_14ae_EVLA_X_final.image.tt0",
-               f"{prefix}/11.0GHz/Target_ASASSN_14ae_EVLA_X_final.image.tt0"]
-               #f"{prefix}/13.55GHz/Target_ASASSN_14ae_EVLA_KU_final.image.tt0",
-               #f"{prefix}/15.08GHz/Target_ASASSN_14ae_EVLA_KU_final.image.tt0",
-               #f"{prefix}/16.62GHz/Target_ASASSN_14ae_EVLA_KU_final.image.tt0",
-               #f"{prefix}/20.06GHz/Target_ASASSN_14ae_EVLA_K_final.image.tt0",
-               #f"{prefix}/22.0GHz/Target_ASASSN_14ae_EVLA_K_final.image.tt0",
-               #f"{prefix}/23.94GHz/Target_ASASSN_14ae_EVLA_K_final.image.tt0"]
-'''
 
 # optional toggles
-combine_results = True
+combine_results = True        # If multiple image paths, will append fit results for each and write to one big csv file for easy copy/pasting
 source_free_region = None     # Specify casa region file or string to use in calculating the near-source RMS. If NONE, default annulus used.
 print_results = True          # if True, prints results as a dictionary in terminal
 write_results = True          # if True, writes results as an excel sheet in imfit_files subdirectory
