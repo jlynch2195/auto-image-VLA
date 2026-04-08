acc678065b4a394f3aea6d9070e98902e8be3eef
Jimmy Lynch <jlyn2195@gmail.com>
2026-04-08 15:23:02 -0700

    added basic fitting for run_fit_point_source.py

--- run_fit_point_source.py
+++ run_fit_point_source.py
@@ -12,7 +12,7 @@ from casatasks import tclean, imfit, imstat, imhead
 # import homemade functions 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, script_dir)
-from helper_functions import fit_point_source
+from helper_functions import fit_point_source, fit_point_source_basic
 
 '''
 prefix = "/Users/jimmylynch/Desktop/radio/observations/20A-492/ASASSN-14ae/final_files"
@@ -34,29 +34,44 @@ image_paths = [f"{prefix}/1.26GHz/Target_AT2023mfm_EVLA_L_final.image.tt0",
                f"{prefix}/10.13GHz/Target_AT2023mfm_EVLA_X_final.image.tt0",
                f"{prefix}/11.06GHz/Target_AT2023mfm_EVLA_X_final.image.tt0"]
 
+# fitting procedure
+fitting_procedure = "basic"
+combine_results = True        # If multiple image paths, will append fit results for each and write to one big csv file for easy copy/pasting
+
+# ======================================================================
+# doesn't matter if using basic procedure
+
 # override the command to fit at the middle of the image
 ra_pix = None
 dec_pix = None
 
 # optional toggles
-combine_results = True        # If multiple image paths, will append fit results for each and write to one big csv file for easy copy/pasting
 source_free_region = None     # Specify casa region file or string to use in calculating the near-source RMS. If NONE, default annulus used.
 print_results = True          # if True, prints results as a dictionary in terminal
 write_results = True          # if True, writes results as an excel sheet in imfit_files subdirectory
 write_regions = True          # if True, writes casa region files for beam, source region of fit, fit result, and source-free region
 override_sfr_request = True   # if True, bypasses the error that will be thrown if the source-free region has a suspected source
 
+# ======================================================================
 # run the script
 results_dicts = []
 for image_path in image_paths:
-    results_dict = fit_point_source(image_path, 
-                                    source_free_region=source_free_region, 
-                                    print_results=print_results, 
-                                    write_results=write_results,
-                                    write_regions=write_regions,
-                                    override_sfr_request=override_sfr_request,
-                                    ra_pix=ra_pix,
-                                    dec_pix=dec_pix)
+    if fitting_procedure == "basic":
+
+        results_dict = fit_point_source_basic(image_path, 
+                                              print_results=print_results, 
+                                              write_results=write_results)
+        
+    else
+        
+        results_dict = fit_point_source(image_path, 
+                                        source_free_region=source_free_region, 
+                                        print_results=print_results, 
+                                        write_results=write_results,
+                                        write_regions=write_regions,
+                                        override_sfr_request=override_sfr_request,
+                                        ra_pix=ra_pix,
+                                        dec_pix=dec_pix)
     results_dicts.append(results_dict)
 
 if combine_results:
